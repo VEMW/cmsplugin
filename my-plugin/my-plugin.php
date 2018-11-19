146b3d79d1dea8699fa9636b2270fa84a351536e
VEMW <34155399+VEMW@users.noreply.github.com>
2018-11-19 11:20:12 +0100

    Add files via upload

---
+++ my-plugin/my-plugin.php
@@ -0,0 +1,12 @@
+/*
+Plugin Name:  my-plugin
+Plugin URI:   https://developer.wordpress.org/plugins/the-basics/
+Description:  mijn eerste plugin
+Version:      1.0
+Author:       Valerie
+Author URI:   https://developer.wordpress.org/
+License:      GPL2
+License URI:  https://www.gnu.org/licenses/gpl-2.0.html
+Text Domain:  wporg
+Domain Path:  /languages
+*/
